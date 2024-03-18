04ba349a16314762e8e8bc42dfb549d8031f9d5b
nilu.walmik@tissatech.com <nilu.walmik@tissatech.com>
2024-03-18 10:18:13 +0530

    first commit1

---
+++ FastlaneCICDApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FastlaneCICDApp
+//
+//  Created by Tissa Macbook on 14/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
